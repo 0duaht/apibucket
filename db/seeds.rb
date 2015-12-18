b4e2fa05acd78ace1e489efd52aa25549cdd525b
Tobi Oduah <tobi.oduah@andela.com>
2015-12-18 07:34:23 +0100

    Fix Hound violations

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,22 +4,22 @@ Item.destroy_all
 
 3.times do |n|
   User.create(
-    name: "User#{n+1}",
-    email: "user#{n+1}@seed.com",
-    password: "user#{n+1}pass"
+    name: "User#{n + 1}",
+    email: "user#{n + 1}@seed.com",
+    password: "user#{n + 1}pass"
   )
 end
 
 9.times do |n|
   Bucketlist.create(
-    name: "Bucketlist #{n+1}",
+    name: "Bucketlist #{n + 1}",
     user_id: (n % 3) + 1
   )
 end
 
 100.times do |n|
   Item.create(
-    name: "Item #{n+1}",
+    name: "Item #{n + 1}",
     bucketlist_id: (n % 10) + 1
   )
 end
